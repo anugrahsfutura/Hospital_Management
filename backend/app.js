1ed2581ed9a506eea2f2a68305fc43226d5a9685
safvan <safvank1998@gmail.com>
2022-06-25 10:36:56 +0530

    authentication

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,40 @@
-require('dotenv').config
-const express=require('express')
-const app=new express()
-const cors=require('cors')
-const port=process.env.PORT || 8080 
+// require("dotenv").config;
+console.log("hello world");
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
 
-const employeeRouter=require('./src/routes/employeeRoutes')
-const departmentRouter=require('./src/routes/departmentRoutes')
+const app = express();
+const router = require("./src/routes/user-routes");
+const cors = require("cors");
+// const port = process.env.PORT || 8080;
 
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-app.use('/employee',employeeRouter)
-app.use('/dept',departmentRouter)
+const employeeRouter = require("./src/routes/employeeRoutes");
+const departmentRouter = require("./src/routes/departmentRoutes");
+// const { signup } = require("./src/controller/user-controller");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+app.use("/api", router);
+// mongoose
+//   .connect(
+//     "mongodb+srv://safvan-123:JFFklSoUiqOmldde@cluster0.jnzxj.mongodb.net/?retryWrites=true&w=majority"
+//   )
+//   .then(() => {
+//     app.listen(8080);
+//     console.log("database is connected to localhost 5000");
+//   })
+//   .catch((err) => console.log(err));
 
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/employee", employeeRouter);
+app.use("/dept", departmentRouter);
 
-app.listen(port,()=>{
-    console.log(`http://localhost:8080`);
-})
+app.listen(8080, () => {
+  console.log(`http://localhost:8080`);
+});
+
+//mongo password
+//JFFklSoUiqOmldde
--- backend/config/connection.js
+++ backend/config/connection.js
@@ -1,42 +1,39 @@
-const mongoose=require('mongoose')
-const url='mongodb://localhost:27017/Hospital'
-mongoose.connect(url,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-    
-})
+const mongoose = require("mongoose");
+//const url = "mongodb://localhost:27017/Hospital";
+const url =
+  "mongodb+srv://safvan-123:JFFklSoUiqOmldde@cluster0.jnzxj.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-const schema=mongoose.Schema
-const DepartmentSchema=new schema({
-    DepartmentName:String,
-    YearFounded:Number,
-    Description:String,
-    Head:String,
-    DepartmentImage:String
+const schema = mongoose.Schema;
+const DepartmentSchema = new schema({
+  DepartmentName: String,
+  YearFounded: Number,
+  Description: String,
+  Head: String,
+  DepartmentImage: String,
+});
+const HeadSchema = new schema({
+  Name: String,
+  Age: Number,
+  ProfileDescription: String,
+  EmployeeNumber: Number,
+  DepartmentName: String,
+  ProfileImage: String,
+});
+const EmployeeSchema = new schema({
+  Name: String,
+  Age: Number,
+  ProfileDescription: String,
+  EmployeeNumber: Number,
+  DepartmentName: String,
+  ProfileImage: String,
+});
 
-})
-const HeadSchema=new schema({
-    Name:String,
-    Age:Number,
-    ProfileDescription:String,
-    EmployeeNumber:Number,
-    DepartmentName:String,
-    ProfileImage:String
-})
-const EmployeeSchema=new schema({
-    Name:String,
-    Age:Number,
-    ProfileDescription:String,
-    EmployeeNumber:Number,
-    DepartmentName:String,
-    ProfileImage:String
+const DepartmentData = mongoose.model("Department", DepartmentSchema);
+const HeadData = mongoose.model("DepartmentHead", HeadSchema);
+const EmployeeData = mongoose.model("Employee", EmployeeSchema);
 
-})
-
-
-const DepartmentData=mongoose.model("Department",DepartmentSchema)
-const HeadData=mongoose.model("DepartmentHead",HeadSchema)
-const EmployeeData=mongoose.model("Employee",EmployeeSchema)
-
-
-module.exports={DepartmentData,HeadData,EmployeeData}
+module.exports = { DepartmentData, HeadData, EmployeeData };
