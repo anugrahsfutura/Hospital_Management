bb5f34c739b17340860e840a13cee524a4dea839
anugrahsfutura <anugrahsfutura@gmail.com>
2022-06-26 18:38:48 +0530

    login style

--- frontend/hospital-app/src/Components/Login/Login.jsx
+++ frontend/hospital-app/src/Components/Login/Login.jsx
@@ -1,29 +1,28 @@
 import React from "react";
 import { Button, Form } from "react-bootstrap";
+import './Login.css'
 
 function Login() {
   return (
-    <div>
-      <Form>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
-
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Login
-        </Button>
-      </Form>
+    <div className="login-box">
+      <h2>LOGIN</h2>
+      <form action="">
+        <div className="user-box">
+          <input type="text" name="" id="" />
+          <label htmlFor="">Username</label>
+        </div>
+        <div className="user-box">
+          <input type="password" name="" id="" />
+          <label htmlFor="">Password</label>
+        </div>
+        <a href="">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          Submit
+        </a>
+      </form>
     </div>
   );
 }
