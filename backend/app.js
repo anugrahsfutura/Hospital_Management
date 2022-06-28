04e1f2cf730155f3553e7540432e8c3232180a37
anugrahs123 <anugrahsasikumar@gmail.com>
2022-06-28 14:22:17 +0530

    staged

--- backend/app.js
+++ backend/app.js
@@ -5,18 +5,15 @@ const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const app = express();
-const router = require("./src/routes/user-routes");
 
 const port = process.env.PORT || 8080;
 
+const router = require("./src/routes/user-routes");
 const employeeRouter = require("./src/routes/employeeRoutes");
 const departmentRouter = require("./src/routes/departmentRoutes");
-<<<<<<< HEAD
-app.use(cors());
-=======
 // const { signup } = require("./src/controller/user-controller");
-app.use(cors({ credentials: true }));
->>>>>>> 80ac702345cfa65200afb10782f77e4e63aef564
+app.use(cors())
+// app.use(cors({ credentials: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
@@ -25,7 +22,7 @@ app.use("/api", router);
 app.use("/employee", employeeRouter);
 app.use("/dept", departmentRouter);
 
-app.listen(port, () => {
+app.listen(8080, () => {
   console.log(`http://localhost:8080`);
 });
 
