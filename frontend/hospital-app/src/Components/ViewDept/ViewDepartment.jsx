5d69b2fb9a66c7670b41d7a7b860f03a545fd963
anugrahs123 <anugrahsasikumar@gmail.com>
2022-07-16 15:50:01 +0530

    last

--- frontend/hospital-app/src/Components/ViewDept/ViewDepartment.jsx
+++ frontend/hospital-app/src/Components/ViewDept/ViewDepartment.jsx
@@ -3,25 +3,43 @@ import React, { useEffect } from "react";
 import { useState } from "react";
 import { Button, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import Search from "../Search/Search";
 
 function ViewDepartment() {
   const [department, setdepartment] = useState([]);
+  const [dept,setdept]=useState([])
 
   const getData = () => {
     axios.get(`http://localhost:8080/dept/view`).then((res) => {
       console.log(res.data);
+      setdept(res.data)
       setdepartment(res.data);
     });
   };
-
+ 
+  const handleSearch=(e)=>{
+    const searchWord=e.target.value
+    console.log(searchWord);
+    const filterdata=department.filter((data)=>data.DepartmentName.toLowerCase().includes(searchWord.toLowerCase()))
+    console.log("filter",filterdata);
+    setdepartment(filterdata)
+    if(searchWord==""){
+      setdepartment(dept)
+    }
+  }
   useEffect(() => {
     getData();
+   
   }, []);
 
   return (
     <div>
 
       <h1 style={{color:"blueviolet"}}>DEPARTMENTS</h1>
+      <div style={{display:'flex',justifyContent:'flex-end',margin:'1rem '}}>
+        <input type="text" placeholder="search" onChange={handleSearch}/>
+      </div>
+      
     <div
       style={{
         justifyContent: "center",
